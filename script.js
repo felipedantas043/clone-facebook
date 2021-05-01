9a1adf4394043674741a8873409dc4865fff3429
Felipe Dantas de Araújo <72935710+felipedantas043@users.noreply.github.com>
2021-04-30 22:34:05 -0300

    lateral esquerda quase completa

---
+++ script.js
@@ -0,0 +1,20 @@
+function ver_mais(){
+    if(document.getElementById('ver_mais_hidden').style.display == "none"){
+        document.getElementById('ver_mais_hidden').style.display = "block"
+        document.getElementById('ver+').innerHTML = "ver menos"
+        document.getElementById('seta-cima').style.display = "block"
+        document.getElementById('seta-baixo').style.display = "none"
+        document.getElementById('options').style.height = "1410px"
+        
+    }else{
+        document.getElementById('ver_mais_hidden').style.display = "none"
+        document.getElementById('ver+').innerHTML = "Ver mais"
+        document.getElementById('seta-cima').style.display = "none"
+        document.getElementById('seta-baixo').style.display = "block"
+        document.getElementById('options').style.height = "350px"
+
+    }
+
+
+    
+}
